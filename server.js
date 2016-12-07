184c185cadc69f835f5b14aae03ba0b48c527fd5
vishnukv100 <vivekkv2120@gmail.com>
2016-12-07 15:23:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',functio(req,res){
+   res.send('article onse is requested and served here') ;
+});
+app.get('/article-two',functio(req,res){
+   res.send('article onse is requested and served here') ;
+});
+app.get('/article-three',functio(req,res){
+   res.send('article onse is requested and served here') ;
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
